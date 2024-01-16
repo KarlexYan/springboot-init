eb00f823508563188faa1ca657dd5dbaaa6b8c7d
KarlexYan <karlexyan@aliyun.com>
2024-01-16 22:37:06 +0800

    init

---
+++ src/main/java/com/karlexyan/yoj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.karlexyan.yoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.karlexyan.yoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
